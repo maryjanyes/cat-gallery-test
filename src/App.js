2fd85a3a7c26299e5e184d226e78d1f494192997
Manfred Xu <manfredxu99@gmail.com>
2020-02-20 07:55:54 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+- Write a React component that fetches the data from the link below 
+and shows a gallery of cats in various moods.
+- When the start or end is reached, the gallery should cycle to the 
+opposite end.
+- The left and right buttons should trigger a sliding animation to the 
+next/previous image 
+(example: https://raw.githubusercontent.com/farzadso/Bootstrap-Carousel/master/preview.gif)
+- The preview panel at the bottom should move one image to the left or 
+right when the buttons are clicked.
+- The gallery can have a maximum width of 500 but should be responsive 
+if the window is made smaller.
+*/
+
+import React from 'react'
+import './App.css'
+
+const link = 'https://gist.githubusercontent.com/manfredxu99/df3be12d855d2e8825d30784a43d4b31/raw/d5efd3062343703df33bf0ec1b0c469fb83cb9f9/cat.json';
+const CHEVRON_LEFT_SRC = 'https://icons.deanishe.net/icon/material/444/arrow-back/256.png'
+const CHEVRON_RIGHT_SRC = 'https://icons.deanishe.net/icon/material/444/arrow-right/256.png'
+
+const CatGallery = () => {
+  return null
 }
 
-export default App;
+export default CatGallery
